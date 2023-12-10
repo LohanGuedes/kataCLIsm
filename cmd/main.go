7b4c32ced2e991cc511a4b888a185ffdc89e571b
Lguedes <lohan.guedes@nutrix.tech>
2023-12-10 05:36:09 -0300

    REVERTING BACK LOL - Frist coomit - kinda

--- cmd/main.go
+++ cmd/main.go
@@ -4,13 +4,215 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/glamour"
+	"github.com/charmbracelet/lipgloss"
 )
 
+const content = `
+# Today’s Menu
+
+## Appetizers
+
+| Name        | Price | Notes                           |
+| ---         | ---   | ---                             |
+| Tsukemono   | $2    | Just an appetizer               |
+| Tomato Soup | $4    | Made with San Marzano tomatoes  |
+| Okonomiyaki | $4    | Takes a few minutes to make     |
+| Curry       | $3    | We can add squash if you’d like |
+
+## Seasonal Dishes
+
+| Name                 | Price | Notes              |
+| ---                  | ---   | ---                |
+| Steamed bitter melon | $2    | Not so bitter      |
+| Takoyaki             | $3    | Fun to eat         |
+| Winter squash        | $3    | Today it's pumpkin |
+
+## Desserts
+
+| Name         | Price | Notes                 |
+| ---          | ---   | ---                   |
+| Dorayaki     | $4    | Looks good on rabbits |
+| Banana Split | $5    | A classic             |
+| Cream Puff   | $3    | Pretty creamy!        |
+
+All our dishes are made in-house by Karen, our chef. Most of our ingredients
+are from our garden or the fish market down the street.
+
+Some famous people that have eaten here lately:
+
+* [x] René Redzepi
+* [x] David Chang
+* [ ] Jiro Ono (maybe some day)
+
+Bon appétit!
+`
+
+type status int
+
+const divisor = 4
+
+const (
+	kataList status = iota
+	readme
+)
+
+/* STYLING */
+var (
+	columnStyle = lipgloss.NewStyle().
+			Padding(1, 2).
+			Border(lipgloss.RoundedBorder()).BorderForeground(lipgloss.Color("#FFF"))
+	focusedStyle = lipgloss.NewStyle().
+			Padding(1, 2).
+			Border(lipgloss.RoundedBorder()).
+			BorderForeground(lipgloss.Color("62"))
+	helpStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("241"))
+)
+
+/* CUSTOM ITEM */
+
+type Kata struct {
+	title       string
+	description string
+}
+
+// implement the list.Item interface
+func (t Kata) FilterValue() string {
+	return t.title
+}
+
+func (t Kata) Title() string {
+	return t.title
+}
+
+func (t Kata) Description() string {
+	return t.description
+}
+
+/* MAIN MODEL */
+
+type Model struct {
+	list     list.Model
+	err      error
+	readme   viewport.Model
+	focused  status
+	loaded   bool
+	quitting bool
+}
+
+func New() *Model {
+	return &Model{}
+}
+
+func (m *Model) Next() {
+	if m.focused == readme {
+		m.focused = kataList
+	} else {
+		m.focused++
+	}
+}
+
+func (m *Model) Prev() {
+	if m.focused == kataList {
+		m.focused = readme
+	} else {
+		m.focused--
+	}
+}
+
+func (m *Model) initLists(width, height int) {
+	defaultList := list.New([]list.Item{}, list.NewDefaultDelegate(), width/divisor, height-divisor)
+	defaultList.SetShowHelp(false)
+	defaultList.InfiniteScrolling = true
+	m.list = defaultList
+
+	// Init To Do
+	m.list.Title = "Available Katas:"
+	m.list.SetItems([]list.Item{
+		Kata{title: "buy milk", description: "strawberry milk"},
+		Kata{title: "fold laundry", description: "or wear wrinkly t-shirts"},
+		Kata{title: "eat sushi", description: "negitoro roll, miso soup, rice"},
+	})
+}
+
+func (m *Model) initViewport(width, height int) {
+	vp := viewport.New(width/divisor*2, height-divisor)
+	vp.Style = lipgloss.NewStyle().
+		BorderStyle(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("62")).
+		PaddingRight(2)
+
+	renderer, err := glamour.NewTermRenderer(
+		glamour.WithAutoStyle(),
+		glamour.WithWordWrap(width),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	str, err := renderer.Render(content)
+	if err != nil {
+		panic(err)
+	}
+
+	vp.SetContent(str)
+	m.readme = vp
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		if !m.loaded {
+			m.initLists(msg.Width, msg.Height)
+			m.initViewport(msg.Width, msg.Height)
+			m.loaded = true
+		}
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			m.quitting = true
+			return m, tea.Quit
+		case "left", "h":
+			m.Prev()
+		case "right", "l":
+			m.Next()
+		}
+	}
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m Model) View() string {
+	if m.quitting {
+		return ""
+	}
+	if m.loaded {
+		kataView := m.list.View()
+		readmeView := m.readme.View()
+		return lipgloss.JoinHorizontal(
+			lipgloss.Left,
+			columnStyle.Render(kataView),
+			columnStyle.Render(readmeView),
+		)
+	} else {
+		return "loading..."
+	}
+}
+
 func main() {
-	p := tea.NewProgram(initialModel())
+	m := New()
+	p := tea.NewProgram(m)
 	if _, err := p.Run(); err != nil {
-		fmt.Printf("ERROR\tThere was an error: %v\n", err)
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
